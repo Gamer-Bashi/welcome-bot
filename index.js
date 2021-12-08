eb694710fb30766072b45fc22905ed4f5aca191a
Gamer-Bashi <95768104+Gamer-Bashi@users.noreply.github.com>
2021-12-08 20:27:22 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client;
+const config = require("./config.json");
+
+client.on("ready", () => {
+    console.log("The bot is ready");
+    client.user.setActivity(`${config.Activity}`, { type: `${config.Type}` })
+});
+
+client.on("guildMemberAdd", async member => {
+    let channel = member.guild.channels.cache.find(c => c.id === `${config.ChannelID}`)
+    let WELCOME = new Discord.MessageEmbed()
+        .setTitle('Welcome')
+        .setDescription(`${member.user} ${config.Descriptione-Embed}`)
+        .setColor('BLUE')
+        .setThumbnail(`${config.Tasvire-Koochik}`)
+        .setImage(`${config.tasvire-bozorh}`)
+        .setTimestamp()
+        .setFooter(`${config.footer}`)
+    channel.send(WELCOME)
+})
+
+client.login(config.token)
